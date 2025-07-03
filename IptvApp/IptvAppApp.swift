156ef92756c4cb3cd5c49b898148c5b456582c21
Daniel Dada <doesdeos@gmail.com>
2025-07-03 11:50:59 +0300

    Initial Commit

---
+++ IptvApp/IptvAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IptvAppApp.swift
+//  IptvApp
+//
+//  Created by Daniel on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct IptvAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
